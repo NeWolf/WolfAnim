83ae72a2d34a44d1fe9b811f94f54f47705a5916
NeWolf <newolf006@126.com>
2024-01-11 14:39:13 +0800

    1.fix dir name

--- WolfAnimLib/src/androidTest/java/com/newolf/anim/ExampleInstrumentedTest.kt
+++ WolfAnimLib/src/androidTest/java/com/newolf/anim/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.ewolf.wolfanimdemo
+package com.ewolf.wolfanim
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.ewolf.wolfanimdemo", appContext.packageName)
+        assertEquals("com.newolf.anim.test", appContext.packageName)
     }
 }
--- app/src/androidTest/java/com/newolf/animdemo/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/newolf/animdemo/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.ewolf.wolfanim
+package com.ewolf.wolfanimdemo
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.ewolf.wolfanim.test", appContext.packageName)
+        assertEquals("com.newolf.animdemo", appContext.packageName)
     }
 }
